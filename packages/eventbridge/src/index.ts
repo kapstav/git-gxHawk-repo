f2708013c2279b6047c2750e64e1f70824848243
Srivastava <ksrivastava@galaxe.com>
2024-03-22 02:12:09 -0700

    KS41

--- packages/eventbridge/src/index.ts
+++ packages/eventbridge/src/index.ts
@@ -1,4 +1,27 @@
-export const handler = async function (event: any, context: any) {
-  console.log("Event received", event);
-  console.log("Hello from eventbridge...");
-};
+import { DynamoDB } from 'aws-sdk';
+
+
+export async function handler(event: any) {
+    const dynamoDB = new DynamoDB.DocumentClient();  
+
+        const now = new Date();
+        // Format date and time to YYYYMMDDHHMISS format
+        const formattedTimestamp = now.toISOString().replace(/[-T:]/g, '').slice(0, 14);
+        const numericTimestamp = parseInt(formattedTimestamp, 10);
+    // Example DynamoDB operation
+    const params: DynamoDB.DocumentClient.PutItemInput = {
+        TableName: 'ShopifyOrderData',
+        Item: {
+            id: numericTimestamp ,
+            shopifyorderid:event.detail.payload.id,
+            shopifyorderdetail: event.detail
+        }
+    };
+
+    await dynamoDB.put(params).promise();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Item inserted successfully' })
+    };
+}
