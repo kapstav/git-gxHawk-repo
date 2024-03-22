d9085daa92e3abe319228c50c31801380876c97a
Srivastava <ksrivastava@galaxe.com>
2024-03-22 05:34:05 -0700

    KS62

--- packages/eventbridge/src/index.ts
+++ packages/eventbridge/src/index.ts
@@ -2,6 +2,6 @@ import { DynamoDB } from 'aws-sdk';
 export const handler = async function (event: any, context: any) {
   const dynamoDB = new DynamoDB.DocumentClient();  
   console.log("Event received", event);
-  console.log("Hello from eventbridge.KS..");
+  console.log("Hello from eventbridge.KS");
 };
 
