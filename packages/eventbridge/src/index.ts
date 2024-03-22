2981faded05e435162a1a7f8308a68de5b490b8e
ekta.srivastava <ektasri@yahoo.com>
2024-03-22 01:33:46 -0700

    KS36

--- packages/eventbridge/src/index.ts
+++ packages/eventbridge/src/index.ts
@@ -1,4 +1,20 @@
-export const handler = async function (event: any, context: any) {
-  console.log("Event received", event);
-  console.log("Hello from eventbridge...");
-};
+import { DynamoDB } from 'aws-sdk';
+
+export async function handler(event: any) {
+    const dynamoDB = new DynamoDB.DocumentClient();
+
+    // Example DynamoDB operation
+    const params: DynamoDB.DocumentClient.PutItemInput = {
+        TableName: 'ShopifyOrderData',
+        Item: {
+            key: '300'
+        }
+    };
+
+    await dynamoDB.put(params).promise();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Item inserted successfully' })
+    };
+}
